d16a3b7404ab802699117a29dd40adacbb329c0a
ilhooq <contact@sphilip.com>
2025-11-21 17:29:52 +0100

    Add dark theme and conditional navbar for logged-in users

--- tests/app/layouts/main.php
+++ tests/app/layouts/main.php
@@ -8,7 +8,7 @@
 $user = $this->params['user'];
 ?>
 <!DOCTYPE html>
-<html>
+<html data-bs-theme="dark">
   <head>
     <meta charset="<?= $this->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,20 +19,23 @@ $user = $this->params['user'];
   <body>
     <nav id="mainnav" class="navbar navbar-expand-lg navbar-dark bg-primary py-0">
       <div class="container">
-        <a class="navbar-brand" href="<?= Piko::getAlias('@web/') ?>">Your company</a>
+        <a class="navbar-brand" href="<?= Piko::getAlias('@web/') ?>">Piko user module</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
+        <?php if (!$user->isGuest()): ?>
         <div id="mainmenu" class="collapse navbar-collapse">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item"><a class="nav-link" href="<?= $this->getUrl('user/default/edit') ?>">Edit profile</a></li>
             <?php if ($user->can('admin')): ?>
             <li class="nav-item"><a class="nav-link" href="<?= $this->getUrl('user/admin/users') ?>">Manage users</a></li>
             <?php endif ?>
-            <li class="nav-item"><a class="nav-link" href="<?= $this->getUrl('site/default/logout') ?>">Logout (<?= $user->getIdentity()->username ?>)</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->getUrl('user/default/logout') ?>">Logout (<?= $user->getIdentity()->username ?>)</a></li>
           </ul>
         </div>
+        <?php endif ?>
+
       </div>
     </nav>
 
